5764e0642bbcaeeeb8ca88ad79a59dd5b4aa417d
Rushikesh <rushikeshdevale2658@gmail.com>
2023-01-09 19:18:35 +0530

    Calculate daily wage

--- src/com/wageCalculation/EmployeeWage.java
+++ src/com/wageCalculation/EmployeeWage.java
@@ -2,12 +2,20 @@ package com.wageCalculation;
 
 public class EmployeeWage {
     public static void main(String[] args) {
+        System.out.println("Welcome to the Employee Wage Computation Program !!");
+        int EMP_RATE_PER_HOUR = 20;
         int IS_FULL_TIME = 1;
+        int empHrs = 0;
+        int empwage = 0;
         double empcheck = Math.floor(Math.random() * 10) % 2;
         if (empcheck == IS_FULL_TIME) {
             System.out.println("Employee is Present");
+            empHrs = 8;
         } else {
             System.out.println("Employee is Absent");
+            empHrs = 0;
         }
+        empwage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage " + empwage);
     }
 }
