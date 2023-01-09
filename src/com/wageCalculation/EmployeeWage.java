80b227e5aa10e6ab5701e60b1b9846347082ce2e
Rushikesh <rushikeshdevale2658@gmail.com>
2023-01-09 19:23:00 +0530

    Part Wage Calculation

--- src/com/wageCalculation/EmployeeWage.java
+++ src/com/wageCalculation/EmployeeWage.java
@@ -5,17 +5,22 @@ public class EmployeeWage {
         System.out.println("Welcome to the Employee Wage Computation Program !!");
         int EMP_RATE_PER_HOUR = 20;
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int empHrs = 0;
         int empwage = 0;
-        double empcheck = Math.floor(Math.random() * 10) % 2;
+        double empcheck = Math.floor(Math.random() * 10) % 3;
         if (empcheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Full time ");
             empHrs = 8;
-        } else {
+        }else if(empcheck==IS_PART_TIME){
+            System.out.println("Employee is Part time");
+            empHrs = 4;
+        }
+        else {
             System.out.println("Employee is Absent");
             empHrs = 0;
         }
         empwage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp wage " + empwage);
+        System.out.println("Emp wage = " + empwage);
     }
 }
