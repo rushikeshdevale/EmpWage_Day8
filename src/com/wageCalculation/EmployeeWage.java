eaf800b91b6322baebe956e2d5c04ee6b1cb25e8
Rushikesh <rushikeshdevale2658@gmail.com>
2023-01-10 12:15:17 +0530

    Employee Wage

--- src/com/wageCalculation/EmployeeWage.java
+++ src/com/wageCalculation/EmployeeWage.java
@@ -9,16 +9,18 @@ public class EmployeeWage {
         int empHrs = 0;
         int empwage = 0;
         double empcheck = Math.floor(Math.random() * 10) % 3;
-        if (empcheck == IS_FULL_TIME) {
-            System.out.println("Employee is Full time ");
-            empHrs = 8;
-        }else if(empcheck==IS_PART_TIME){
-            System.out.println("Employee is Part time");
-            empHrs = 4;
-        }
-        else {
-            System.out.println("Employee is Absent");
-            empHrs = 0;
+        switch ((int) empcheck) {
+            case 2:
+                System.out.println("Employee is Part time ");
+                empHrs = 4;
+                break;
+            case 1:
+                System.out.println("Employee is Full time ");
+                empHrs = 8;
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                empHrs = 0;
         }
         empwage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp wage = " + empwage);
