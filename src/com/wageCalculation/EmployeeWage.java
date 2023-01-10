2c6b057f48c49d9108b3decdd20b38110e35aed6
Rushikesh <rushikeshdevale2658@gmail.com>
2023-01-10 12:25:40 +0530

    Total Wage

--- src/com/wageCalculation/EmployeeWage.java
+++ src/com/wageCalculation/EmployeeWage.java
@@ -4,6 +4,9 @@ public class EmployeeWage {
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
+    public static int MAX_WORKING_DAYS = 20;
+    public static final int MAX_WORKING_HRS = 100;
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation Program !!");
@@ -11,7 +14,10 @@ public class EmployeeWage {
         int empHrs = 0;
         int empwage = 0;
         int totalWage = 0;
-        for (int i = 1; i <= workingDay; i++) {
+        int totalWorkingHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalWorkingHrs <= MAX_WORKING_HRS && totalWorkingHrs < MAX_WORKING_DAYS) {
+            totalWorkingDays++;
             int empcheck = (int) (Math.floor(Math.random() * 10) % 3);
             switch (empcheck) {
                 case 2:
@@ -26,10 +32,13 @@ public class EmployeeWage {
                     System.out.println("Employee is Absent");
                     empHrs = 0;
             }
+            totalWorkingHrs += empHrs;
             empwage = empHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Day " + i + " Employee Wage = " + empwage);
+            System.out.println(" Employee Wage = " + empwage);
             totalWage = totalWage + empwage;
+            System.out.println("Total working hrs = " + totalWorkingHrs);
+            System.out.println("Total Emp wage for 20 days = " + totalWage);
         }
-        System.out.println("Total Emp wage for 20 days = " + totalWage);
+
     }
 }
